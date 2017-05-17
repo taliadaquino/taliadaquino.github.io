3d21f02e06a8f15576c74660b0ecdec621b66c79
taliadaquino <taliadaquino@Talias-MBP.lan>
2017-05-17 11:09:02 +1000

    hovers

---
+++ menu/assets/js/main.js
@@ -0,0 +1,41 @@
+$(document).ready(function() {
+
+
+// Add jQuery here
+
+$('.nav').mouseenter(function() {
+  $('.menu').addClass('showme');
+});
+
+
+$('.nav').mouseleave(function() {
+  $('.menu').removeClass('showme');
+});
+
+
+$('.nav').click(function() {
+  $('.menu').addClass('showme');
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  });
---
+++ reader/assets/js/main.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+
+
+// Add jQuery here
+
+$('.one').mouseenter(function() {
+  $('.one__text').addClass('showme');
+});
+
+
+$('.one').mouseleave(function() {
+  $('.one__text').removeClass('showme');
+});
+
+
+
+
+
+
+
+  });
